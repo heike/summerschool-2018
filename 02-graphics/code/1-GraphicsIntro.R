32ee8a6374308ce2b9a3baf0967e4efe38590f8d
Heike Hofmann <hofmann@iastate.edu>
2018-05-15 07:13:03 -0500

    code

---
+++ 02-graphics/code/1-GraphicsIntro.R
@@ -0,0 +1,66 @@
+## ---- echo=FALSE---------------------------------------------------------
+library(ggplot2)
+
+## ---- eval = FALSE-------------------------------------------------------
+## curl::curl_download(
+##   "https://raw.githubusercontent.com/heike/summerschool-2018/master/02-graphics/code/0-Setup.R",
+##   "0-Setup.R"
+## )
+## file.edit("0-Setup.R")
+
+## ---- echo=FALSE---------------------------------------------------------
+library("dlstats")
+
+dframe <- cran_stats("ggplot2")
+ggplot(data = dframe[-nrow(dframe),], aes(x = end, y = downloads)) + geom_point() + geom_line() +
+  ylab("Number of monthly downloads") + xlab("Date") +
+  ggtitle("Number of monthly downloads of ggplot2")
+
+## ------------------------------------------------------------------------
+head(diamonds)
+
+## ---- fig.height=4, fig.width=7------------------------------------------
+qplot(carat, price, geom = "point", data = diamonds)
+
+## ---- fig.height=4, fig.width=7------------------------------------------
+qplot(carat, price, geom = "point", data = diamonds, alpha = I(0.05))
+
+## ---- fig.height=3, fig.width=7------------------------------------------
+qplot(log(carat), log(price), geom = "point", data = diamonds, 
+	colour = color, 
+	main = "Log price by carat weight, grouped by color") + 
+    xlab("Log Carat Weight") + ylab("Log Price")
+
+## ------------------------------------------------------------------------
+iowa <- read.csv("https://raw.githubusercontent.com/heike/summerschool-2018/master/02-graphics/data/brfss-iowa-2012.csv")
+
+## ---- fig.height=4, fig.width=7------------------------------------------
+qplot(price, geom = "histogram", data = diamonds)
+
+## ---- fig.height=4, fig.width=7------------------------------------------
+qplot(price, geom = "histogram",  binwidth = 50, data = diamonds)
+
+## ---- fig.height=4, fig.width=7------------------------------------------
+qplot(price, geom = "histogram", data = diamonds, binwidth = 100, facets = .~clarity)
+
+## ---- fig.height=4, fig.width=7------------------------------------------
+qplot(clarity, log(price), geom = "boxplot", data = diamonds)
+
+## ---- fig.height=3, fig.width=7------------------------------------------
+qplot(clarity, log(price), geom = "boxplot", data = diamonds, 
+	main = "Boxplots of log Diamond Prices Grouped by Clarity") +
+	geom_jitter(alpha = I(.025))
+
+## ---- fig.height=4, fig.width=7------------------------------------------
+qplot(clarity, log(price)/carat, geom = "boxplot", data = diamonds)
+
+## ------------------------------------------------------------------------
+titanic <- as.data.frame(Titanic)
+
+## ---- fig.height=4, fig.width=7------------------------------------------
+qplot(Survived, geom = "bar", data = titanic, weight = Freq)
+
+## ---- fig.height=4, fig.width=7------------------------------------------
+qplot(Survived, geom = "bar", data = titanic, weight = Freq, 
+      facets = Sex~Class)
+
